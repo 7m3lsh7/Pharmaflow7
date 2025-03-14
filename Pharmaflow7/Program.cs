9aee5986e9c1594727ce822a741bf98ec66e35bd
77m3lsh7 7 <felopater.remon2020@gmail.com>
2025-03-15 00:25:38 +0200

    55

--- Pharmaflow7/Program.cs
+++ Pharmaflow7/Program.cs
@@ -55,6 +55,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=dashboard}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
