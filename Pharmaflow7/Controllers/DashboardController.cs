c14241627db255fde49f42001720ac9e94730195
Christina Ramzy <christinaramzy30@gmail.com>
2025-03-14 22:18:42 +0200

    kk

---
+++ Pharmaflow7/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pharmaflow7.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
