62b4d80c81066e4faa4e9c83c05983a2e741d23d
77m3lsh7 7 <felopater.remon2020@gmail.com>
2025-03-20 22:50:05 +0200

    maked Company

---
+++ Pharmaflow7/Models/CompanyDashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Pharmaflow7.Models
+{
+    public class CompanyDashboardViewModel
+    {
+
+        public int TotalProducts { get; set; }
+        public int ActiveShipments { get; set; }
+        public int PerformanceScore { get; set; }
+        public List<object> Shipments { get; set; }
+        public List<object> SalesData { get; set; }
+        public List<object> DistributionData { get; set; }
+
+    }
+}
